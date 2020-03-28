f2a57d4a055f72b6da699a0913fb846d3019f20b
strongishrope <eric.sun.ca@gmail.com>
2020-03-28 13:48:33 -0400

    aws pipeline draft

---
+++ src/main/java/com/brightcase/Beanstalk1Application.java
@@ -0,0 +1,13 @@
+package com.brightcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beanstalk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beanstalk1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/brightcase/Beanstalk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brightcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beanstalk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
